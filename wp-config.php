9673d5cd8f6c4287365327552217ed146db0b18b
MalabarDesign <jhubert@malabardesign.fr>
2016-10-17 21:53:09 +0200

    push

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'srv020.serveur-dedie.fr');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
